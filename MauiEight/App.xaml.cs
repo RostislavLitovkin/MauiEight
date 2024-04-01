6c5495b6e8a1e7e2f86cadb60184c29664a33fde
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-04-01 19:21:56 +0200

    Add project files.

---
+++ MauiEight/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiEight
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiEight/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiEight
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
